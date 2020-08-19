5ee63143c4d65ea1d77a6520152fe01bd54c14ca
Ray Raya <ray@Rays-MacBook-Pro.local>
2020-08-19 19:13:48 +0900

    Modify app.js to dynamically fix card sizes

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-let words = $(".card-text p").text();
-let shortenedChar = words.substring(0, 100);
+alert("hi!");
 
-$(".card-text p").innerHTML = shortenedChar + "..." ;
+let allCards = document.querySelectorAll(".card-text");
+let wordArray = [];
+let linkArr = [
+  'https://rayraya.github.io/UdemyTinDog',
+  'https://rayraya.github.io/DrumKit/',
+  'https://rayraya.github.io/SimonGame/',
+  'https://todolists-raya.herokuapp.com/',
+  'https://newsletters-raya.herokuapp.com/',
+  'https://blog-raya.herokuapp.com/'
+];
+
+let index = 0;
+for(card of allCards){
+  wordArray.push(card.innerText);
+  let shortenedChar = card.innerText.substring(0, 100);
+  card.innerHTML = shortenedChar +  "...<a href =" + linkArr[index] + " target = '_blank'/><br />Read more.</a>";
+  index++;
+}
+
+console.log(wordArray);
