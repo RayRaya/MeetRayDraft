d65912b7c0122ffa1b6d9b83805fbc1445c66262
Ray Raya <ray@Rays-MacBook-Pro.local>
2020-08-19 20:16:24 +0900

    Add functionality, and titles to cards

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-alert("hi!");
-
 let allCards = document.querySelectorAll(".card-text");
 let wordArray = [];
 let linkArr = [
@@ -14,9 +12,10 @@ let linkArr = [
 let index = 0;
 for(card of allCards){
   wordArray.push(card.innerText);
-  let shortenedChar = card.innerText.substring(0, 100);
-  card.innerHTML = shortenedChar +  "...<a href =" + linkArr[index] + " target = '_blank'/><br />Read more.</a>";
+  let text = card.innerText;
+  let shortenedChar = "";
+  text.length <= 100? shortenedChar = text: shortenedChar = text.substring(0, 90) + "...";
+  card.innerHTML = shortenedChar +
+    "<br /><div class = 'row'> <a class = 'col-8' href = '#' target = '_blank'/>Read more.</a> <a class = 'col-4' href = "  + linkArr[index] +  " target = '_blank'>Link</a></div>";
   index++;
 }
-
-console.log(wordArray);
