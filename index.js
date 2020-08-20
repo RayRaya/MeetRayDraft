edd18ec734477a60d3deb81865ec916976b88213
Ray Raya <ray@Rays-MacBook-Pro.local>
2020-08-20 12:33:06 +0900

    Add two new react projects in the portfolio section

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ let linkArr = [
   'https://rayraya.github.io/SimonGame/',
   'https://todolists-raya.herokuapp.com/',
   'https://newsletters-raya.herokuapp.com/',
-  'https://blog-raya.herokuapp.com/'
+  'https://blog-raya.herokuapp.com/',
+  'https://85bt8.csb.app/',
+  'https://tywxd.csb.app/'
 ];
 
 let index = 0;
@@ -16,6 +18,11 @@ for(card of allCards){
   let shortenedChar = "";
   text.length <= 100? shortenedChar = text: shortenedChar = text.substring(0, 90) + "...";
   card.innerHTML = shortenedChar +
-    "<br /><div class = 'row'> <a class = 'col-8' href = '#' target = '_blank'/>Read more.</a> <a class = 'col-4' href = "  + linkArr[index] +  " target = '_blank'>Link</a></div>";
+    "<br /><div class = 'row'> <a onclick = 'clickReadMore(" + this.value + ")' value = '1' class = 'col-8' href = '#' />Read more <span style = 'font-size: 11px;'>&#9660;</span></a> <a class = 'col-4' href = "  + linkArr[index] +  " target = '_blank'>Link</a></div>";
   index++;
+}
+
+function clickReadMore(index){
+  alert('its been called');
+  alert(index);
 }
